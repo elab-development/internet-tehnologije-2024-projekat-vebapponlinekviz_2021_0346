cf53bca4e0857da5d2487dce5e45fcaaac88bc22
Kacii02 <nikolic.katarina002@gmail.com>
2025-02-06 18:31:14 +0100

    Server podignut i povezan sa bazom.

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const port = 2812;
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+mongoose.connect('mongodb+srv://kacii:rfDXMnkAOeOoZk2W@backend.c88zu.mongodb.net/?retryWrites=true&w=majority&appName=backend')
+  .then(() => {
+    console.log('Connected!');
+    app.listen(port, ()=>{
+      console.log("Server is running on port "+port);
+    })
+    
+  })
+  .catch(()=> {
+    console.log("Connection failed!");
+  })
