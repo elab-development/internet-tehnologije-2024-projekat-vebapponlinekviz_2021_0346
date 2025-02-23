78782b8da090158d369f87b6c1bcd31d15500b0c
markoarsyc <markoarsyc@gmail.com>
2025-02-23 19:29:18 +0100

    Uradjena logika za register formu u Reactu.

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,20 +1,62 @@
 import React, { useState } from "react";
 import "./styles/Register.css";
 
+const Register = () => {
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-const Register = () => {  
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    let newUser = {
+      name,
+      mail,
+      username,
+      password
+    }
+    console.log(newUser);
+  }
 
   return (
     <div className="register-wrapper">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="ime">Ime:</label>
-        <input type="text" name="ime" />
+        <input
+          type="text"
+          name="ime"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
         <label htmlFor="mail">Mail:</label>
-        <input type="text" name="mail" />
+        <input
+          type="text"
+          name="mail"
+          value={mail}
+          onChange={(e) => {
+            setMail(e.target.value);
+          }}
+        />
         <label htmlFor="korisnicko-ime">Korisničko ime:</label>
-        <input type="text" name="korisnicko-ime" />
+        <input
+          type="text"
+          name="korisnicko-ime"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
         <label htmlFor="lozinka">Lozinka:</label>
-        <input type="password" name="lozinka" />
+        <input
+          type="password"
+          name="lozinka"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
         <label htmlFor="ponovljena-lozinka">Ponovljena lozinka:</label>
         <input type="password" name="ponovljena-lozinka" />
         <button type="submit">KREIRAJ NALOG</button>
