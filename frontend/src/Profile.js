3eed875cb19d32f88554959099a6c0ef12dc3ec8
Kacii02 <nikolic.katarina002@gmail.com>
2025-05-23 17:46:23 +0200

    Dodata stranica za profil

---
+++ frontend/src/Profile.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./media/Logo.png";
+import "./styles/Profile.css";
+
+const Profile = () => {
+  return (
+    <div className="profile-wrapper">
+      <div className="profile-wrapper-left">
+        <img src={logo} alt="logo" />
+        <div className="profile-points">
+          <p>Kategorija 1: {}</p>
+          <p>Kategorija 2: {}</p>
+          <p>Kategorija 3: {}</p>
+          <p>Kategorija 4: {}</p>
+        </div>
+      </div>
+      <div className="profile-wrapper-right">
+        <div className="profile-user-data">
+            <p>Ime: {}</p>
+            <p>Mail: {}</p>
+            <p>Korisničko ime: {}</p>
+            <button>Izmeni podatke</button>
+        </div>
+        <button id="DELETE-DATA">Obriši podatke</button>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
