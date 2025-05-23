3c8590f9fd0a739417b0972ed14dcd7ced38634d
markoarsyc <markoarsyc@gmail.com>
2025-05-24 00:19:25 +0200

    Omoguceno dinamicko prosledjivanje stila za kategorije

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import Cateogory from './Cateogory';
 import Login from './Login';
 
 function App() {
+  const category1 = {
+    number:1,
+    name:"Kategorija1"
+  }
+  const category2 = {
+    number:2,
+    name:"Kategorija2"
+  }
+  const category3 = {
+    number:3,
+    name:"Kategorija3"
+  }
+  const category4 = {
+    number:4,
+    name:"Kategorija4"
+  }
   return (
     <div className="App">
-      <Login />
+      <Cateogory category={category1}/>
     </div>
   );
 }
--- frontend/src/Cateogory.js
+++ frontend/src/Cateogory.js
@@ -1,28 +1,42 @@
 import React from "react";
-import smallIcon from "./media/C1small.png";
-import bigIcon from "./media/C1big.png";
 import "./styles/Category.css";
+import C1big from './media/C1big.png';
+import C2big from './media/C2big.png';
+import C3big from './media/C3big.png';
+import C4big from './media/C4big.png';
+import C1small from './media/C1small.png';
+import C2small from './media/C2small.png';
+import C3small from './media/C3small.png';
+import C4small from './media/C4small.png';
+import C1bg from './media/C1bg.png';
+import C2bg from './media/C2bg.png';
+import C3bg from './media/C3bg.png';
+import C4bg from './media/C4bg.png';
 
-const Cateogory = () => {
+const Cateogory = ({category}) => {
+    const bigIcons = [C1big,C2big,C3big,C4big];
+    const smallIcons = [C1small,C2small,C3small,C4small];
+    const backgrounds = [C1bg,C2bg,C3bg,C4bg];
+    const colors = ["#00c3ff","#F3ADDF", "#FFA007", "#1F1A53"];
   return (
-    <div className="category-wrapper">
+    <div className="category-wrapper" style={{backgroundImage: `url(${backgrounds[category.number - 1]})`}}>
       <div className="category-heading">
         <div className="category-name">
-          <img src={smallIcon} alt="small-icon" />
-          <h2>Kategorija 1</h2>
+          <img src={smallIcons[category.number - 1]} alt="small-icon" />
+          <h2>{category.name}</h2>
         </div>
-        <div className="category-timer">29</div>
+        <div className="category-timer" style={{backgroundColor: `${colors[category.number - 1]}`}}>29</div>
       </div>
       <div className="category-questions">
-        <div className="category-question">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla est ex, fermentum et nulla tristique, rhoncus interdum ligula. Etiam egestas pretium imperdiet. Suspendisse sit amet scelerisque mauris. Cras tortor ante.</div>
+        <div className="category-question" style={{backgroundColor: `${colors[category.number - 1]}`}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla est ex, fermentum et nulla tristique, rhoncus interdum ligula. Etiam egestas pretium imperdiet. Suspendisse sit amet scelerisque mauris. Cras tortor ante.</div>
         <div className="category-answers">
-          <button className="answer answer1">Odgovor 1</button>
-          <button className="answer answer2">Odgovor 2</button>
-          <button className="answer answer3">Odgovor 3</button>
-          <button className="answer answer4">Odgovor 4</button>
+          <button className="answer answer1" style={{backgroundColor: `${colors[category.number - 1]}`}}>Odgovor 1</button>
+          <button className="answer answer2" style={{backgroundColor: `${colors[category.number - 1]}`}}>Odgovor 2</button>
+          <button className="answer answer3" style={{backgroundColor: `${colors[category.number - 1]}`}}>Odgovor 3</button>
+          <button className="answer answer4" style={{backgroundColor: `${colors[category.number - 1]}`}}>Odgovor 4</button>
         </div>
       </div>
-      <img src={bigIcon} alt="big-icon" id="BIG-ICON"/>
+      <img src={bigIcons[category.number - 1]} alt="big-icon" id="BIG-ICON"/>
     </div>
   );
 };
