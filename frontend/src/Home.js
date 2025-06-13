12e728a5cf8580abe9bbbd5887eca89dc4e87e67
Kacii02 <nikolic.katarina002@gmail.com>
2025-06-13 17:22:55 +0200

    Odradjen prikaz najboljih rezultata po kategoriji za igraca

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,46 +2,70 @@ import React, { useContext, useEffect, useState } from "react";
 import logo from "./media/Logo.png";
 import shark from "./media/Shark.png";
 import "./styles/Home.css";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { LoginContext } from "./context/LoginContext";
 import axios from "axios";
 
-const Home = ({setCurrentCategory}) => {
+const Home = ({ setCurrentCategory }) => {
   const navigate = useNavigate();
 
   const api = axios.create({
     baseURL: "http://localhost:2812/api/",
   });
 
-  const {player} = useContext(LoginContext);
+  const { player } = useContext(LoginContext);
 
   const [allCategories, setAllCategories] = useState([]);
+  const [bestScores, setBestScores] = useState([]);
 
-  const handleNavigate = ()=> {
+  const handleNavigate = () => {
     navigate("/profile");
-  }
+  };
 
-  const getAllCategories = async ()=> {
+  const getAllCategories = async () => {
     return await api.get("/categories");
-  }
+  };
 
-  useEffect(()=>{
-    const fetchCategories = async ()=> {
+  const getBestScoresByCategory = async (playerId) => {
+    return await api.get(`/games/best/${playerId}`);
+  };
+
+  useEffect(() => {
+    const getScores = async () => {
+      if (player) {
+        let scores = await getBestScoresByCategory(player._id);
+        setBestScores(scores.data);
+        console.log("Skor: " + scores.data);
+      }
+    };
+    getScores();
+  }, [player]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
       let categories = await getAllCategories();
       setAllCategories(categories.data);
-    }
+    };
     fetchCategories();
-  },[])
+  }, []);
 
   return (
     <div className="home-wrapper">
       <div className="left-wrapper">
         <img src={logo} alt="logo" />
         <div className="points">
-          <p>Kategorija 1: {}</p>
-          <p>Kategorija 2: {}</p>
-          <p>Kategorija 3: {}</p>
-          <p>Kategorija 4: {}</p>
+          {player ? (
+            bestScores?.map((item, index) => (
+              <p key={index}>
+                {item.categoryTitle}: {item.score}
+              </p>
+            ))
+          ) : (
+            <>
+              <p><Link to="/login">Ulogujte</Link> se za prikaz poena</p>
+              <p>ili <Link to="/register">napravite profil</Link></p>
+            </>
+          )}
         </div>
       </div>
       <div className="right-wrapper">
@@ -50,11 +74,19 @@ const Home = ({setCurrentCategory}) => {
           <img src={shark} alt="shark" />
         </div>
         <div className="category-button-wrapper">
-          {allCategories?.map((category, index)=> {
-            return <button key={index} className={"category-button " + "button-" + index} onClick={()=>{
-              setCurrentCategory(category);
-              navigate("/game");
-            }}>{category.title}</button>
+          {allCategories?.map((category, index) => {
+            return (
+              <button
+                key={index}
+                className={"category-button " + "button-" + index}
+                onClick={() => {
+                  setCurrentCategory(category);
+                  navigate("/game");
+                }}
+              >
+                {category.title}
+              </button>
+            );
           })}
         </div>
       </div>
